ce321dd73ef18d0e9cc55102cbb1ced8b2a954a6
Anirudh Sharma <anirudh1304@gmail.com>
2022-06-26 20:27:33 +0530

    API update

--- server.js
+++ server.js
@@ -1,66 +1,63 @@
-const path = require('path')
-const ejs = require('ejs')
-const axios = require('axios')
-const express = require('express')
+const path = require("path");
+const ejs = require("ejs");
+const axios = require("axios");
+const express = require("express");
+const qs = require("qs");
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 //static folder
-app.use(express.static(path.join(__dirname, 'public')))
-
-
+app.use(express.static(path.join(__dirname, "public")));
 
 //routes
-app.get('', (req,res)=> {
-    res.render('index')
-})
+app.get("", (req, res) => {
+  res.render("index");
+});
 
-app.get('/About_Us', (req,res)=> {
-    res.render('about')
-})
+app.get("/About_Us", (req, res) => {
+  res.render("about");
+});
 
-app.get('/CodeMate/:roomid', (req,res)=> {
-    res.render('codemate', {
-        id: req.params.roomid
+app.get("/CodeMate/:roomid", (req, res) => {
+  res.render("codemate", {
+    id: req.params.roomid,
+  });
+});
+
+app.get("/answer", (req, res) => {
+  const reqData = JSON.parse(req.query.submit);
+
+  var data = qs.stringify({
+    code: reqData.code,
+    language: reqData.language,
+    input: reqData.input,
+  });
+
+  var config = {
+    method: "post",
+    url: "https://codex-api.herokuapp.com/",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      res.send(JSON.stringify(response.data));
     })
-})
+    .catch(function (error) {
+      res.send(error);
+    });
+});
 
-app.get('/answer', (req,res)=> {
-    console.log('abc')
-    if(!req.query.submit) {
-        return alert('Invalid Submission')
-    }
-    else {
-            let config = {
-            method: 'post',
-            url: 'https://codexweb.netlify.app/.netlify/functions/enforceCode',
-            headers: { 
-                'Content-Type': 'application/json'
-            },
-            data : req.query.submit
-            };
-        
-            axios(config)
-            .then(function (response) {
-                console.log(response.data)
-                let output = response.data
-                res.json({output})
-            })
-            .catch(function (error) {
-                res.send(error)
-            });
-    }
-})
+app.get("*", (req, res) => {
+  res.render("404");
+});
 
-app.get('*', (req,res)=> {
-    res.render('404')
-})
-
-
-
-app.listen(PORT, ()=> {
-    console.log(`server running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
